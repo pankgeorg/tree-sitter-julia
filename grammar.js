c77c1a13d91d5f0a14a1322c87e526b54a113f50
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliahub.com>
2026-04-17 13:14:18 +0300

    Revert "fix(grammar): keep var_identifier in macro_identifier for @var"...""
    This reverts commit 58a7d1a73f4f47fa2daec4da9b8dead19c499f19.

--- grammar.js
+++ grammar.js
@@ -1039,7 +1039,6 @@ module.exports = grammar({
       $.operator,
       alias($._syntactic_operator, $.operator),
       alias($._scoped_identifier, $.field_expression),
-      $.var_identifier, // @var"..."
     )),
 
     _scoped_identifier: $ => seq(
